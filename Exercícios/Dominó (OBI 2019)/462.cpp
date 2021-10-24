bced9a9658ed5d4064c19e1542aaa7b7904387b5
Gustavo Valença <gustavovalenca15@gmail.com>
2021-10-24 11:30:30 -0300

    Create 462.cpp

---
+++ Exercícios/Dominó (OBI 2019)/462.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int pieces = (n + 1) * (n + 2) / 2;
+
+    cout << pieces << endl;
+
+    return 0;
+}
